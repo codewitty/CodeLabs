40f1b7756cd6f97b4d3afa81c50b071ae3daebcd
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-05 11:48:55 -0800

    new fns

--- constructors.cpp
+++ constructors.cpp
@@ -39,4 +39,13 @@ class Accumulator {
             sum += a;
         }
 };
-/*
+/* Write a full class definition for a class named GasTank, and containing the following members:
+
+A data member named amount of type double.
+A constructor that accepts no parameters. The constructor initializes the data member amount to 0.
+A function named addGas that accepts a parameter of type double. The value of the amount instance variable is increased by the value of the parameter.
+A function named useGas that accepts a parameter of type double. The value of the amount data member is decreased by the value of the parameter.
+A function named getGasLevel that accepts no parameters. getGasLevel returns the value of the amount data member.
+*/
+
+
