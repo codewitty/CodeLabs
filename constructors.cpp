eb7858afea31c4b049af70c5a7bd6207dad01ab9
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-05 11:53:17 -0800

    new fns

--- constructors.cpp
+++ constructors.cpp
@@ -71,8 +71,10 @@ class GasTank {
 A data member named amount of type double.
 A constructor that accepts no parameters. The constructor initializes the data member amount to 0.
 A function named addGas that accepts a parameter of type double. The value of the amount instance variable is increased by the value of the parameter.
-A function named useGas that accepts a parameter of type double. The value of the amount data member is decreased by the value of the parameter. However, if the value of amount is decreased below 0, amount is set to 0.
-A function named isEmpty that accepts no parameters and returns a boolean value. isEmpty returns a boolean value: true if the value of amount is less than 0.1, and false otherwise.
+A function named useGas that accepts a parameter of type double. The value of the amount data member is decreased by the value of the parameter. 
+However, if the value of amount is decreased below 0, amount is set to 0.
+A function named isEmpty that accepts no parameters and returns a boolean value. isEmpty returns a boolean value: 
+true if the value of amount is less than 0.1, and false otherwise.
 A function named getGasLevel that accepts no parameters. getGasLevel returns the value of the amount data member.
 */
 
