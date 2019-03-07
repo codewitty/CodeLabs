cc255ec0ea8d724b555eb07ba0e09925a33e05e2
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-07 12:03:24 -0800

    new fns

--- separatingInterface.cpp
+++ separatingInterface.cpp
@@ -76,4 +76,50 @@ class Counter {
 };
 #endif
 
+/*
+Write the implementation (.cpp file) of the Counter class from the previous exercise. Here is the full specification of the class:
+A data member counter of type int.
+A constructor that takes one int argument and assigns its value to counter
+A function called increment that accepts no parameters and returns no value. increment adds one to the counter data member.
+A function called decrement that accepts no parameters and returns no value. decrement subtracts one to the counter data member.
+A function called getValue that accepts no parameters. It returns the value of the instance variable counter.
+*/
+
+int counter;
+
+Counter::Counter(int count) {
+    counter = count;
+}
+
+void Counter::increment(){
+    counter += 1;
+}
+void Counter::decrement() {
+    counter -= 1;
+}
+
+int Counter::getValue() {
+    return counter;
+}
+
+/*
+Write the interface (.h file) of a class Accumulator containing:
+A data member named sum of type integer.
+A constructor accepting an integer parameter.
+A function named getSum that accepts no parameters and returns an integer.
+A function named add that accepts an integer parameter and returns no value.
+*/
+#ifndef ACCUMULATOR_H
+#define ACCUMULATOR_H
+
+class Accumulator {
+    private:
+        int sum;
+    public:
+        Accumulator(int count);
+        void add(int);
+        int getSum() ;
+};
+#endif
+
 
