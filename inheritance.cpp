963efb1a7b1d0f5318d0c6d8714bfc47eb4eafd9
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-19 10:51:28 -0700

    new fns

--- inheritance.cpp
+++ inheritance.cpp
@@ -26,3 +26,8 @@ WindowWithBorder::WindowWithBorder(int width) {
 int WindowWithBorder::getUseableWidth(){
     return (windowWidth - borderWidth);
 }
+
+/*
+Assume the existence of a Phone class. Define a derived class, CameraPhone that contains two data members: an integer named, imageSize, representing the size in megabytes of each picture, and an integer named memorySize, representing the number of megabytes in the camera's memory. There is a constructor that accepts two integer parameters corresponding to the above two data members and which are used to initialize the respective data members. There is also a function named numPictures that returns (as an integer) the number of pictures the camera's memory can hold.
+*/
+
