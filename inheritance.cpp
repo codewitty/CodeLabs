1425574decbedac1c9be863f00ba14eaf04bca04
Joshua Gomes <gomesjoshua@gmail.com>
2019-03-19 10:49:11 -0700

    new fns

---
+++ inheritance.cpp
@@ -0,0 +1,4 @@
+/*
+Assume the existence of a Window class with a function getWidth that returns the width of the window. Define a derived class WindowWithBorder that contains a single additional integer instance variable named borderWidth, and has a constructor that accepts an integer parameter which is used to initialize the instance variable. There is also a function getUseableWidth, that returns the width of the window minus the width of the border.
+*/
+
